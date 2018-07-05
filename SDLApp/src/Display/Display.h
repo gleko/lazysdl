8df0ec01857007c21a3fbeb414f28ca2c1998b79
Gergely Leko-Rozsa <gergely.leko-rozsa@ericsson.com>
2018-07-05 15:31:13 +0200

    Restructure of classes started

---
+++ SDLApp/src/Display/Display.h
@@ -0,0 +1,13 @@
+namespace screen {
+    //Screen dimension constants
+    const int SCREEN_WIDTH = 1280;
+    const int SCREEN_HEIGHT = 720;
+
+
+
+    class Display
+    {
+        Display();
+        ~Display() = default;
+    };
+}
